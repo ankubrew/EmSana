5fdd77d5ab3946964681fd282cbe4d7e0266b6a1
Anku <kuanyshbai.sanzjar@gmail.com>
2026-01-27 19:49:43 +0500

    added login and decipher of passwords

--- EmSana/main.py
+++ EmSana/main.py
@@ -2,8 +2,19 @@ from typing import List, Optional
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 from fastapi import FastAPI, HTTPException, Depends
 from datetime import datetime
+from passlib.context import CryptContext
 
-# === 1. НАСТРОЙКА БАЗЫ ДАННЫХ ===
+# Шифрование паролей
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+#  НАСТРОЙКА БАЗЫ ДАННЫХ
 sqlite_file_name = "emsana.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
@@ -22,14 +33,17 @@ app = FastAPI()
 def on_startup():
     create_db_and_tables()
 
-# === 2. ТАБЛИЦЫ (МОДЕЛИ) ===
+# ТАБЛИЦЫ (МОДЕЛИ)
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+    # unique=True означает: "В базе не может быть двух одинаковых ИИН"
+    iin: str = Field(unique=True)  # БЫЛО inn, СТАЛО iin
     first_name: str
     last_name: str
-    role: str  # "doctor" или "patient"
+    role: str
     doctor_id: Optional[int] = Field(default=None)
+    password: str
 
 class DailyLog(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -46,7 +60,7 @@ class Medication(SQLModel, table=True):
     time: str
     is_taken: bool = False
 
-# === 3. ЭНДПОИНТЫ (API) ===
+# ЭНДПОИНТЫ (API) 
 
 @app.get("/")
 def read_root():
@@ -55,6 +69,7 @@ def read_root():
 # --- Регистрация / Создание юзера ---
 @app.post("/users/", response_model=User)
 def create_user(user: User, session: Session = Depends(get_session)):
+    user.password = get_password_hash(user.password)
     session.add(user)
     session.commit()
     session.refresh(user)
@@ -98,9 +113,38 @@ def add_medication(med: Medication, session: Session = Depends(get_session)):
     session.refresh(med)
     return med
 
+# Создаем модель данных специально для входа (Login)
+class LoginRequest(SQLModel):
+    iin: str   # БЫЛО inn, СТАЛО iin
+    password: str
+
+@app.post("/login")
+def login(request: LoginRequest, session: Session = Depends(get_session)):
+    # 1. Ищем человека по ИИН
+    statement = select(User).where(User.iin == request.iin) # БЫЛО inn, СТАЛО iin
+    user = session.exec(statement).first()
+    
+    # 2. Если такого нет ИЛИ пароль не подходит
+    if not user or not verify_password(request.password, user.password):
+        return {"status": "error", "message": "Неверный логин или пароль"}
+    
+    # 3. Если всё ок
+    return {
+        "status": "success", 
+        "user_id": user.id, 
+        "role": user.role,
+        "first_name": user.first_name
+    }
+
 # --- Пациент: Мои лекарства ---
 @app.get("/medications/{patient_id}", response_model=List[Medication])
 def get_patient_meds(patient_id: int, session: Session = Depends(get_session)):
     statement = select(Medication).where(Medication.patient_id == patient_id)
     results = session.exec(statement)
     return results.all()
+
+
+#запуск
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
